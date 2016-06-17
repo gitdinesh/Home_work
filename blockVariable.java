a7f6440d4f43a98611e014693b52caedae6610a2
gitdinesh <dinesh.abey123@gmail.com>
2016-06-17 21:32:43 +0530

    new java block variabale

---
+++ blockVariable.java
@@ -0,0 +1,24 @@
+public class blockVariable{
+
+	public static void main(String[] args) {
+		
+		int x =10;
+
+		if(x>=4){
+
+
+				//user define block
+
+			String s = "x";
+
+			System.out.println(s);
+		}	else{
+
+
+			System.out.println("x less than 4");
+
+
+		}
+	}
+
+}
